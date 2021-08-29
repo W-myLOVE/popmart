d215093aeab68d5937b2628f447f62cf7c8d1ec8
W-myLOVE <962820604@qq.com>
2021-08-29 09:23:32 +0800

    高德地图引入

--- src/views/Map/childcomp/mapcontent.vue
+++ src/views/Map/childcomp/mapcontent.vue
@@ -1,18 +1,33 @@
 <template>
- <div class="content">
- </div>
+  <div class="box">
+    <div id="container" style="width:100vw; height:35vw"></div>
+  </div>
 </template>
 
 <script>
+import AMap from 'AMap' // 引入高德地图
 export default {
-  components: {
+  mounted () {
+    this.init()
   },
-  data () {
-    return {
+  methods: {
+    init () {
+     var map = new AMap.Map('container', {
+        resizeEnable: true,
+        center: [116.397428, 39.90923],
+        zoom: 13,
+         viewMode:'3D'//使用3D视图
+    });
+    //实时路况图层
+   var marker = new AMap.Marker({
+        position:[116.39, 39.9]//位置
+    })
+    map.add(marker);//添加到地图
+
     }
+     
   }
 }
-
 </script>
 <style scoped>
 .content{
